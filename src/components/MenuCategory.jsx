6a5601c6b0b285f87414145f4fc97133c414953d
Gurugokul <gurugokul05@gmail.com>
2025-09-09 07:32:41 +0530

    Updated loading animation while fetching data from db

--- src/components/MenuCategory.jsx
+++ src/components/MenuCategory.jsx
@@ -11,7 +11,7 @@ import { FaShoppingCart } from "react-icons/fa";
 const MenuCategory = () => {
   const [foodItems, setFoodItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
-
+  const [loading,setLoading] = useState(true)
   const { category } = useParams();
   // console.log(category)
   useEffect(() => {
@@ -24,6 +24,7 @@ const MenuCategory = () => {
       }));
 
       setFoodItems(items);
+      setLoading(false);
     };
     fetchData();
   }, [category]);
@@ -77,83 +78,101 @@ const MenuCategory = () => {
     
     
   };
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+  else{
 
-  return (
-    <div>
-      <Helmet>
-        <title>Yoga Family Restaurant</title>
-      </Helmet>
-      <header>
+    return (
+      <div>
+        <Helmet>
+          <title>Yoga Family Restaurant</title>
+        </Helmet>
+        <header>
+          <div>
+            <div>
+              <h1>Yoga Family Restaurant</h1>
+            </div>
+          </div>
+        </header>
         <div>
           <div>
-            <h1>Yoga Family Restaurant</h1>
+            <div id="food-list">
+              {foodItems.map((item) => (
+                <div key={item.id} className="items">
+                  <img src={item.img} alt={item.name} />
+                  <p>{item.name}</p>
+                  <p>
+                    ₹{item.price} - {item.status}
+                  </p>
+                  <button
+                    disabled={item.status === "Unavailable"}
+                    onClick={() => handleCart(item)}
+                  >
+                    {item.status === "Available" ? "Add to Cart" : "Unavailable"}
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
+          <button id="cart-sticky-button">
+            <Link to="/cart">
+              <FaShoppingCart size={40} />
+            </Link>
+          </button>
         </div>
-      </header>
-      <div>
-        <div>
-          <div id="food-list">
-            {foodItems.map((item) => (
-              <div key={item.id} className="items">
-                <img src={item.img} alt={item.name} />
-                <p>{item.name}</p>
-                <p>
-                  ₹{item.price} - {item.status}
-                </p>
-                <button
-                  disabled={item.status === "Unavailable"}
-                  onClick={() => handleCart(item)}
+        <div id="footer">
+          <footer>
+            <h2>Contact Information</h2>
+            <br />
+            <ul>
+              <li>Phone Number : 99943 19875 , 87542 58484.</li>
+              <br />
+  
+              <li>
+                Address : No 789 , Yoga Tower , Near Kalasalingam University ,
+                Krishnankoil.
+              </li>
+              <br />
+              <li>
+                <a
+                  href="https://maps.app.goo.gl/vErHfsb1LtGLzQgHA"
+                  target="_blank"
                 >
-                  {item.status === "Available" ? "Add to Cart" : "Unavailable"}
-                </button>
-              </div>
-            ))}
-          </div>
+                  Gmap Link
+                </a>
+              </li>
+              <br />
+              <li>
+                For orders Call the above numbers. Home Delivery Upto 3 KM.{" "}
+              </li>
+              <br />
+              <li>Opening Hours : 10 A.M to 10 P.M .</li>
+              <br />
+  
+              <br />
+              <li>© 2025 Yoga Family Restaurant. All rights reserved.</li>
+              <br />
+              <li>Created by Gurugokul & Team .</li>
+            </ul>
+          </footer>
         </div>
-        <button id="cart-sticky-button">
-          <Link to="/cart">
-            <FaShoppingCart size={40} />
-          </Link>
-        </button>
       </div>
-      <div id="footer">
-        <footer>
-          <h2>Contact Information</h2>
-          <br />
-          <ul>
-            <li>Phone Number : 99943 19875 , 87542 58484.</li>
-            <br />
+    );
+  }
 
-            <li>
-              Address : No 789 , Yoga Tower , Near Kalasalingam University ,
-              Krishnankoil.
-            </li>
-            <br />
-            <li>
-              <a
-                href="https://maps.app.goo.gl/vErHfsb1LtGLzQgHA"
-                target="_blank"
-              >
-                Gmap Link
-              </a>
-            </li>
-            <br />
-            <li>
-              For orders Call the above numbers. Home Delivery Upto 3 KM.{" "}
-            </li>
-            <br />
-            <li>Opening Hours : 10 A.M to 10 P.M .</li>
-            <br />
-
-            <br />
-            <li>© 2025 Yoga Family Restaurant. All rights reserved.</li>
-            <br />
-            <li>Created by Gurugokul & Team .</li>
-          </ul>
-        </footer>
-      </div>
-    </div>
-  );
 };
 
 export default MenuCategory;
