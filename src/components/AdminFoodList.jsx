c5f5efb1857dde8cdd84258f9030aca63d02135d
Gurugokul <gurugokul05@gmail.com>
2025-09-09 22:16:30 +0530

    Image Changes

--- src/components/AdminFoodList.jsx
+++ src/components/AdminFoodList.jsx
@@ -26,97 +26,97 @@ const AdminFoodList = () => {
         </div>
         <div id="food-list">
           <div className="items">
-            <img src="./src/assets/IMG_20250719_191256.jpg" alt="Biryani" />
+            <img src="/IMG_20250719_191256.jpg" alt="Biryani" />
             <p>Biryani</p>
             <button onClick={() => handleView("Biryani")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/parrota.jpg" alt="Parotta & Chappathi" />
+            <img src="/parrota.jpg" alt="Parotta & Chappathi" />
             <p>Parotta & Chappathi</p>
             <button onClick={() => handleView("Parotta")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/naan.jpg" alt="Rotti/Naan" />
+            <img src="/naan.jpg" alt="Rotti/Naan" />
             <p>Rotti/Naan</p>
             <button onClick={() => handleView("Naan")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/fried_rice.jpg" alt="Fried Rice" />
+            <img src="/fried_rice.jpg" alt="Fried Rice" />
             <p>Fried Rice</p>
             <button onClick={() => handleView("Fried-Rice")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/noodles.jpg" alt="Fried Noodles" />
+            <img src="/noodles.jpg" alt="Fried Noodles" />
             <p>Fried Noodles</p>
             <button onClick={() => handleView("Fried-Noodles")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/dosa.jpg" alt="Dosa" />
+            <img src="/dosa.jpg" alt="Dosa" />
             <p>Dosa</p>
             <button onClick={() => handleView("Dosa")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/chicken_gravy.jpg" alt="Chicken Gravy" />
+            <img src="/chicken_gravy.jpg" alt="Chicken Gravy" />
             <p>Chicken Gravy</p>
             <button onClick={() => handleView("Chicken-Gravy")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/egg_gravy.jpg" alt="Egg" />
+            <img src="/egg_gravy.jpg" alt="Egg" />
             <p>Egg</p>
             <button onClick={() => handleView("Egg")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/veg_gravy.jpg" alt="Veg Gravy" />
+            <img src="/veg_gravy.jpg" alt="Veg Gravy" />
             <p>Veg Gravy</p>
             <button onClick={() => handleView("Veg-Gravy")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/grill_chicken.jpg" alt="Grill/Tandoori" />
+            <img src="/grill_chicken.jpg" alt="Grill/Tandoori" />
             <p>Grill/Tandoori</p>
             <button onClick={() => handleView("Grill")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/soup.jpg" alt="Soup" />
+            <img src="/soup.jpg" alt="Soup" />
             <p>Soup</p>
             <button onClick={() => handleView("Soup")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/starters.jpg" alt="Starters" />
+            <img src="/starters.jpg" alt="Starters" />
             <p>Starters</p>
             <button onClick={() => handleView("Starters")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/burger.jpg" alt="Burger/Sandwich" />
+            <img src="/burger.jpg" alt="Burger/Sandwich" />
             <p>Burger/Sandwich</p>
             <button onClick={() => handleView("Burger")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/shawarma.jpg" alt="Shawarma/Rolls/Wraps" />
+            <img src="/shawarma.jpg" alt="Shawarma/Rolls/Wraps" />
             <p>Shawarma/Rolls/Wraps</p>
             <button onClick={() => handleView("Shawarma")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/pizza.jpg" alt="Pizza" />
+            <img src="/pizza.jpg" alt="Pizza" />
             <p>Pizza</p>
             <button onClick={() => handleView("Pizza")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/fried_chicken.jpg" alt="Fried Chicken" />
+            <img src="/fried_chicken.jpg" alt="Fried Chicken" />
             <p>Fried Chicken</p>
             <button onClick={() => handleView("Fried-Chicken")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/juice.jpg" alt="Mojito/Fresh Juice" />
+            <img src="/juice.jpg" alt="Mojito/Fresh Juice" />
             <p>Mojito/Fresh Juice</p>
             <button onClick={() => handleView("Fresh-Juice")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/icecream.jpg" alt="Ice Creams" />
+            <img src="/icecream.jpg" alt="Ice Creams" />
             <p>Ice Creams</p>
             <button onClick={() => handleView("Ice-Creams")}>View</button>
           </div>
           <div className="items">
-            <img src="./src/assets/icecream.jpg" alt="Milk Shakes" />
+            <img src="/icecream.jpg" alt="Milk Shakes" />
             <p>Milk Shakes</p>
             <button onClick={() => handleView("Milk-Shakes")}>View</button>
           </div>
--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -42,17 +42,17 @@ const AdminPanel = () => {
         <h1 style={{textAlign:"center", marginTop: "20px"}}>Welcome to Admin Pannel</h1>
       <div id="food-list" style={{ marginTop: "20px" }}>
         <div className="items">
-          <img src="./src/assets/view_orders.jpeg" alt="" style={{borderRadius:"20%"}} />
+          <img src="/view_orders.jpeg" alt="" style={{borderRadius:"20%"}} />
           <p>View Orders</p>
           <button onClick={() => viewOrders()}>View Orders</button>
         </div>
         <div className="items">
-          <img src="./src/assets/edit_icon.jpeg" alt="" />
+          <img src="/edit_icon.jpeg" alt="" />
           <p>Edit Status</p>
           <button onClick={() => editStatus()}>Edit Status</button>
         </div>
         <div className="items">
-          <img src="./src/assets/change_price.jpeg" alt="" />
+          <img src="/change_price.jpeg" alt="" />
           <p>Change Price</p>
           <button onClick={() => changePrice()}>Change price</button>
         </div>
